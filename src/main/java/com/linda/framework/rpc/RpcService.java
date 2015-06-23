6699c6f53163227564b64200aaa447284f4d65d2
linda <linsony0@163.com>
2015-06-23 12:03:23 +0800

    misc:fix equal method

--- src/main/java/com/linda/framework/rpc/RpcService.java
+++ src/main/java/com/linda/framework/rpc/RpcService.java
@@ -50,6 +50,43 @@ public class RpcService implements Serializable{
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((impl == null) ? 0 : impl.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RpcService other = (RpcService) obj;
+		if (impl == null) {
+			if (other.impl != null)
+				return false;
+		} else if (!impl.equals(other.impl))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "RpcMonitorBean [name=" + name + ", version=" + version
 				+ ", impl=" + impl + "]";
--- src/main/java/com/linda/framework/rpc/cluster/RpcHostAndPort.java
+++ src/main/java/com/linda/framework/rpc/cluster/RpcHostAndPort.java
@@ -35,4 +35,32 @@ public class RpcHostAndPort {
 		return host + ":" + port;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + port;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RpcHostAndPort other = (RpcHostAndPort) obj;
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
+			return false;
+		if (port != other.port)
+			return false;
+		return true;
+	}
+
 }
